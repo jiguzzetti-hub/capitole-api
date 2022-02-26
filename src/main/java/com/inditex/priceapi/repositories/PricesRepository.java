7d84492c4d77e23d572a78d1c18ff59fd79a3480
jiguzzetti-hub <juanignacio.guzzetti@mercadolibre.com>
2022-02-26 11:22:56 -0300

    remove dao and fix service and query logic

--- src/main/java/com/inditex/priceapi/repositories/PricesRepository.java
+++ src/main/java/com/inditex/priceapi/repositories/PricesRepository.java
@@ -10,10 +10,30 @@ import java.util.List;
 
 public interface PricesRepository extends JpaRepository<Prices, Long> {
 
-    @Query("select p from Prices p where p.start_date < :datetime and p.end_date > :datetime" +
+    @Query("select p from Prices p where p.start_date <= :datetime and p.end_date >= :datetime" +
             " and p.brand_id = :brand_id and p.product_id = :product_id" +
-            " order by p.priority")
+            " order by p.priority desc")
     List<Prices> findPricesByDatetime(@Param("datetime") LocalDateTime datetime,
-                                      @Param("product_id") String productId,
-                                      @Param("brand_id") String brandId);
+                                      @Param("product_id") int productId,
+                                      @Param("brand_id") int brandId);
+
+    @Query("select p from Prices p where p.brand_id = :brand_id and p.product_id = :product_id" +
+            " and p.priority > :priority and p.start_date <= :end_date and p.start_date >= :datetime" +
+            " order by p.start_date asc, p.priority desc")
+    List<Prices> findEndDate(@Param("datetime") LocalDateTime datetime,
+                             @Param("product_id") int productId,
+                             @Param("brand_id") int brandId,
+                             @Param("end_date") LocalDateTime endDate,
+                             @Param("priority") int priority);
+
+    @Query("select p from Prices p where p.brand_id = :brand_id and p.product_id = :product_id" +
+            " and p.priority > :priority and p.end_date >= :start_date and p.end_date <= :datetime" +
+            " and p.start_date <= :datetime and p.end_date >= :datetime" +
+            " order by p.end_date desc , p.priority desc")
+    List<Prices> findStartDate(@Param("datetime") LocalDateTime datetime,
+                             @Param("product_id") int productId,
+                             @Param("brand_id") int brandId,
+                             @Param("start_date") LocalDateTime startDate,
+                             @Param("priority") int priority);
+
 }
--- src/main/java/com/inditex/priceapi/services/PricesService.java
+++ src/main/java/com/inditex/priceapi/services/PricesService.java
@@ -1,29 +1,55 @@
 package com.inditex.priceapi.services;
 
 import com.inditex.priceapi.dtos.ItemPriceByDateResponse;
+import com.inditex.priceapi.entities.Prices;
+import com.inditex.priceapi.repositories.PricesRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
 
-import static com.inditex.priceapi.services.ProductIdUtil.validateProductId;
-
+@Service
 public class PricesService {
+
+    private final PricesRepository pricesRepository;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PricesService.class);
 
+    public PricesService(PricesRepository pricesRepository) {
+        this.pricesRepository = pricesRepository;
+    }
 
-    public static ResponseEntity<?> getItemPriceByDatetime(String brandId, String productId, String incomingDatetime) {
+
+    public Optional<ItemPriceByDateResponse> getItemPriceByDatetime(int brandId, int productId, LocalDateTime datetime) {
         ItemPriceByDateResponse response = new ItemPriceByDateResponse();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        LocalDateTime dateTime = LocalDateTime.parse(incomingDatetime, formatter);
-        if (validateProductId(productId)){
-
-
-            return ResponseEntity.ok(response);
+        List<Prices> itemList = pricesRepository.findPricesByDatetime(datetime, productId, brandId);
+        if (itemList.isEmpty()) {
+            return Optional.empty();
         }
-        return ResponseEntity.badRequest().body(response);
+        response.setBrandId(brandId);
+        response.setProductId(productId);
+        response.setPriceList(itemList.get(0).getPrice_list());
+        response.setPrice(itemList.get(0).getPrice());
+        List<Prices> itemListForEndDate = pricesRepository.findEndDate(datetime, productId, brandId, itemList.get(0).getEnd_date(),
+                itemList.get(0).getPriority());
+        if (!itemListForEndDate.isEmpty()) {
+            response.setToDate(itemListForEndDate.get(0).getStart_date());
+            List<Prices> itemListForStartDate = pricesRepository.findStartDate(datetime, productId, brandId,
+                    itemList.get(0).getStart_date(), itemList.get(0).getPriority());
+            if (!itemListForStartDate.isEmpty()) {
+                response.setFromDate(itemListForStartDate.get(0).getEnd_date());
+                return Optional.of(response);
+            }
+            response.setFromDate(itemList.get(0).getStart_date());
+            return Optional.of(response);
+        }
+        response.setFromDate(itemList.get(0).getStart_date());
+        response.setToDate(itemList.get(0).getEnd_date());
+        return Optional.of(response);
+
+
     }
 }
